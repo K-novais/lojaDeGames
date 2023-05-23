5f495cc099147f806a1008293c9811be3ecb8dcb
unknown <kellynamaral@yahoo.com>
2023-05-23 02:41:07 -0300

    first commit

---
+++ src/main/java/com/generation/lojaDeGames/controller/CategoriaController.java
@@ -0,0 +1,29 @@
+package com.generation.lojaDeGames.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.generation.lojaDeGames.model.Categoria;
+import com.generation.lojaDeGames.repository.CategoriaRepository;
+
+@RestController
+@RequestMapping("/ categorias")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
+public class CategoriaController {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
+	@GetMapping
+	public ResponseEntity<List<Categoria>> getAll(){
+		return ResponseEntity.ok(categoriaRepository.findAll());
+		
+	}
+	
+	}
---
+++ src/main/java/com/generation/lojaDeGames/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package com.generation.lojaDeGames.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.generation.lojaDeGames.model.Categoria;
+
+public interface CategoriaRepository  extends JpaRepository <Categoria, Long>{
+
+}
